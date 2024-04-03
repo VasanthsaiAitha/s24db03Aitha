24799327e1db77aeb1bea43d598b6438c678ee65
VasanthsaiAitha <aithavasanthsai@gmail.com>
2024-04-03 18:46:02 -0500

    Final commit

--- controllers/MusicalInstruments.js
+++ controllers/MusicalInstruments.js
@@ -44,5 +44,24 @@ exports.MusicalInstruments_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
-
+    // Handle Costume create on POST.
+    exports.MusicalInstruments_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new MusicalInstruments();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.instrument_name = req.body.instrument_name;
+    document.condition = req.body.condition;
+    document.price = req.body.price;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
     
