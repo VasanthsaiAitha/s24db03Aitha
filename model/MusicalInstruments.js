ccee607164be4bf9a5ec7bcd0f9095b356c225f9
VasanthsaiAitha <aithavasanthsai@gmail.com>
2024-04-15 21:19:20 -0500

    added protected end points

--- model/MusicalInstruments.js
+++ model/MusicalInstruments.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose")
 const MusicalInstrumentsSchema = mongoose.Schema({
-instrument_name : String,
+instrument_name : {
+  type:String,
+  minlength:[0],
+  maxlength:[20]
+},
 condition : String,
-price: Number
+price: {
+    type: Number,
+    min: [0],
+    max: [10000]
+  }
 })
 module.exports = mongoose.model("MusicalInstruments",
 MusicalInstrumentsSchema)
--- routes/MusicalInstruments.js
+++ routes/MusicalInstruments.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -17,16 +18,28 @@ router.get('/', MusicalInstruments_controllers.MusicalInstruments_view_all_Page 
 // GET request for one MusicalInstruments.
 router.get('/MusicalInstruments/:id', MusicalInstruments_controllers.MusicalInstruments_detail);
 
-router.put('/',function(req, req){
-if(req.body.checkboxsale) toUpdate.sale = true;
-else toUpdate.same = false;})
+// router.put('/',function(req, req){
+// if(req.body.checkboxsale) toUpdate.sale = true;
+// else toUpdate.same = false;})
 
 /* GET detail MusicalInstruments page */
 router.get('/detail', MusicalInstruments_controllers.MusicalInstruments_view_one_Page);
 /* GET create MusicalInstruments page */
 router.get('/create', MusicalInstruments_controllers.MusicalInstruments_create_Page);
 /* GET create update page */
-router.get('/update', MusicalInstruments_controllers.MusicalInstruments_update_Page);
+const secured = (req, res, next) => {
+  if (req.user){
+  return next();
+  }
+  res.redirect("/login");
+  }
+router.get('/update',secured, MusicalInstruments_controllers.MusicalInstruments_update_Page);
 /* GET delete MusicalInstruments page */
 router.get('/delete', MusicalInstruments_controllers.MusicalInstruments_delete_Page);
+// A little function to check if we have an authorized user and continue on or
+// redirect to login.
+router.post('/login', passport.authenticate('local'), function(req, res) {
+  res.redirect('/');
+  });
+  
 module.exports = router;
